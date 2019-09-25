9e8dec0799cdd44b1e466bbf7ab53b10124083ce
Rayzr522 <rayzr522@gmail.com>
2019-09-25 13:06:24 -0400

    Added config commands and tab-completion

--- src/main/java/me/rayzr522/expflattener/command/CommandExpFlattener.java
+++ src/main/java/me/rayzr522/expflattener/command/CommandExpFlattener.java
@@ -4,8 +4,15 @@ import me.rayzr522.expflattener.ExpFlattener;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 
-public class CommandExpFlattener implements CommandExecutor {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommandExpFlattener implements CommandExecutor, TabCompleter {
     private final ExpFlattener plugin;
 
     public CommandExpFlattener(ExpFlattener plugin) {
@@ -13,14 +20,18 @@ public class CommandExpFlattener implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-//        if (!(sender instanceof Player)) {
-//            sender.sendMessage(plugin.tr("command.fail.only-players"));
-//            return true;
-//        }
-//
-//        Player player = (Player) sender;
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            return Stream.of("version", "reload", "help", "level-requirement", "level-cap")
+                    .filter(sub -> sub.startsWith(args[0].toLowerCase()))
+                    .collect(Collectors.toList());
+        }
 
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!plugin.checkPermission(sender, "use", true)) {
             return true;
         }
@@ -39,6 +50,52 @@ public class CommandExpFlattener implements CommandExecutor {
             case "reload":
                 plugin.reload();
                 sender.sendMessage(plugin.tr("command.expflattener.reloaded"));
+                break;
+            case "level-requirement":
+                if (args.length < 2) {
+                    sender.sendMessage(plugin.tr("command.expflattener.level-requirement.get", plugin.getLevelRequirement()));
+                    break;
+                }
+
+                int newLevelRequirement;
+                try {
+                    newLevelRequirement = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(plugin.tr("error.command.not-number", args[1]));
+                    break;
+                }
+
+                if (newLevelRequirement < 1) {
+                    newLevelRequirement = 1;
+                }
+
+                plugin.getConfig().set("level-requirement", newLevelRequirement);
+                plugin.saveConfig();
+
+                sender.sendMessage(plugin.tr("command.expflattener.level-requirement.set", newLevelRequirement));
+                break;
+            case "level-cap":
+                if (args.length < 2) {
+                    sender.sendMessage(plugin.tr("command.expflattener.level-cap.get", plugin.getLevelCap()));
+                    break;
+                }
+
+                int newLevelCap;
+                try {
+                    newLevelCap = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(plugin.tr("error.command.not-number", args[1]));
+                    break;
+                }
+
+                if (newLevelCap < 1) {
+                    newLevelCap = 1;
+                }
+
+                plugin.getConfig().set("level-cap", newLevelCap);
+                plugin.saveConfig();
+
+                sender.sendMessage(plugin.tr("command.expflattener.level-cap.set", newLevelCap));
                 break;
             case "help":
             case "?":
